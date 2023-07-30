e4616de74ce2645c0b0ac61faa0d1e2f2b7cda70
Lukas Bestle <mail@lukasbestle.com>
2023-07-30 21:31:51 +0200

    Update version number

--- GoCardless.lua
+++ GoCardless.lua
@@ -26,7 +26,7 @@
 -- SOFTWARE.
 
 WebBanking({
-    version = 1.00,
+    version = 1.01,
     url = "https://api.gocardless.com/",
     services = { "GoCardless" },
     description = string.format(MM.localizeText("Get balance and transactions for %s"), "GoCardless"),
